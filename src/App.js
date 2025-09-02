712ea5fbd9a5280bd7f37363d5705d4a47d6403a
Anderson miguel costa <anderson_costa_miguel@hotmail.com>
2025-09-02 11:53:23 -0300

    updates home

--- src/App.js
+++ src/App.js
@@ -2,50 +2,54 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import { ref, get } from "firebase/database";
-import { db } from './database/firebaseConfig';
-import { motion } from 'framer-motion';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import GroupsIcon from '@mui/icons-material/Groups';
-import SecurityIcon from '@mui/icons-material/Security';
-import '../src/styles/main.css'
-
+import { db } from "./database/firebaseConfig";
+import { motion } from "framer-motion";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SecurityIcon from "@mui/icons-material/Security";
+import "../src/styles/main.css";
 
 function App() {
   const navigate = useNavigate();
   const auth = getAuth();
   const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  const fetchCodigo = async () => {
-    try {
-      const codigo = localStorage.getItem('codigo');
-      if (codigo) {
-        // Corrigido: agora navega para a rota correta com o parâmetro
-        navigate(`/portaria/home/${codigo}`);
+  useEffect(() => {
+    const fetchCodigo = async () => {
+      try {
+        const codigo = localStorage.getItem("codigo");
+        if (codigo) {
+          // Corrigido: agora navega para a rota correta com o parâmetro
+          navigate(`/portaria/home/${codigo}`);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar o código:", error);
       }
-    } catch (error) {
-      console.error('Erro ao buscar o código:', error);
-    }
-  };
+    };
 
-  fetchCodigo();
-}, [navigate]);
-
+    fetchCodigo();
+  }, [navigate]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         try {
-          const moradorRef = ref(db, `DadosBelaVista/usuarios/usuarioMorador/${user.uid}`);
+          const moradorRef = ref(
+            db,
+            `DadosBelaVista/usuarios/usuarioMorador/${user.uid}`
+          );
           const moradorSnapshot = await get(moradorRef);
-  
-          const sindicoRef = ref(db, `DadosBelaVista/usuarios/usuarioCondominio/${user.uid}`);
+
+          const sindicoRef = ref(
+            db,
+            `DadosBelaVista/usuarios/usuarioCondominio/${user.uid}`
+          );
           const sindicoSnapshot = await get(sindicoRef);
-  
+
           if (moradorSnapshot.exists()) {
-            navigate('/moradores/home');
+            navigate("/moradores/home");
           } else if (sindicoSnapshot.exists()) {
-            navigate('/sindico/home');
+            navigate("/sindico/home");
           } else {
             await signOut(auth);
             alert("Erro: Tipo de usuário não reconhecido.");
@@ -59,7 +63,7 @@ useEffect(() => {
         setLoading(false);
       }
     });
-  
+
     return () => unsubscribe();
   }, [auth, navigate]);
 
@@ -73,66 +77,92 @@ useEffect(() => {
   }
 
   return (
-  <div className="gradientBackground">
+    <div className="gradientBackground">
       <div className="app-container">
-      <div className="content-container">
-        {/* Logo Centralizado */}
-        <motion.div 
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-          className="logo-container"
-        >
-          <img
-            className="logo"
-            src="/logo.png"
-            alt="Logo do Condomínio"
-          />
-        </motion.div>
-        
-        <motion.div 
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.4 }}
-        >
-          <h1 className="welcome-title">Bem-vindo ao Sistema do Condomínio</h1>
-          <p className="welcome-subtitle">Selecione seu tipo de acesso</p>
-        </motion.div>
-
-        {/* Botões de Acesso */}
-        <motion.div 
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          className="buttons-container"
-        >
-          <button 
-            onClick={() => navigate('/registro-condominio/login')}
-            className="access-button"
+        <div className="content-container">
+          {/* Logo Centralizado */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className="logo-container"
           >
-            <AdminPanelSettingsIcon className="button-icon" />
-            <span className="button-text">Administração</span>
-          </button>
+            <img className="logo" src="/logo.png" alt="Logo do Condomínio" />
+          </motion.div>
 
-          <button 
-            onClick={() => navigate('/registro-morador/login')}
-            className="access-button"
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
           >
-            <GroupsIcon className="button-icon" />
-            <span className="button-text">Moradores</span>
-          </button>
+            <h1 className="welcome-title">
+              Bem-vindo ao Sistema do Condomínio
+            </h1>
+            <p className="welcome-subtitle">Selecione seu tipo de acesso</p>
+          </motion.div>
 
-          <button 
-            onClick={() => navigate('/registro-portaria/login')}
-            className="access-button"
+          {/* Botões de Acesso */}
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            className="buttons-container"
           >
-            <SecurityIcon className="button-icon" />
-            <span className="button-text">Portaria</span>
-          </button>
-        </motion.div>
+            <button
+              onClick={() => navigate("/registro-condominio/login")}
+              className="access-button"
+            >
+            <AdminPanelSettingsIcon 
+  className="button-icon"
+  sx={{ 
+    fontSize: '28px !important', 
+    color: 'rgba(255, 255, 255, 0.7) !important',
+    marginRight: '15px !important',
+    width: '35px !important',
+    height: '35px !important'
+  }}
+/>
+              <span className="button-text">Administração</span>
+            </button>
+
+            <button
+              onClick={() => navigate("/registro-morador/login")}
+              className="access-button"
+            >
+            <GroupsIcon 
+  className="button-icon"
+  sx={{ 
+    fontSize: '32px !important', 
+    color: 'rgba(255, 255, 255, 0.7) !important',
+    marginRight: '15px !important',
+    width: '35px !important',
+    height: '35px !important'
+  }}
+/>
+
+              <span className="button-text">Moradores</span>
+            </button>
+
+            <button
+              onClick={() => navigate("/registro-portaria/login")}
+              className="access-button"
+            >
+         <SecurityIcon 
+  className="button-icon"
+  sx={{ 
+    fontSize: '32px !important', 
+    color: 'rgba(255, 255, 255, 0.7) !important',
+    marginRight: '15px !important',
+    width: '35px !important',
+    height: '35px !important'
+  }}
+/>
+              <span className="button-text">Portaria</span>
+            </button>
+          </motion.div>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
